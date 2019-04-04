b976a5603ce85c6552f2132f28eedea1b05435ac
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-04 22:25:06 +0530

    Create a deck and display it on the screen

---
+++ src/Deck.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './main.css';
+
+class Deck extends React.Component {
+  render() {
+    return <div className="deck">{'\u{1F0A0}'}</div>;
+  }
+}
+
+export default Deck;
