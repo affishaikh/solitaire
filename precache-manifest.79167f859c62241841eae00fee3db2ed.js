d4bb584a37831a04ea9423323b39de91967762fe
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-09 09:30:38 +0530

    Updates

---
+++ precache-manifest.79167f859c62241841eae00fee3db2ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "f270b7c0e7a389686cbe",
+    "url": "/solitaire/static/js/main.74c3de42.chunk.js"
+  },
+  {
+    "revision": "5e44cd857d4f997abac3",
+    "url": "/solitaire/static/js/2.a401ab6f.chunk.js"
+  },
+  {
+    "revision": "f270b7c0e7a389686cbe",
+    "url": "/solitaire/static/css/main.9bfd82f2.chunk.css"
+  },
+  {
+    "revision": "73f2b77bacdba5e465d0cf013d2e4119",
+    "url": "/solitaire/index.html"
+  }
+];
